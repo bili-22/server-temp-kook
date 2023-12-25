8a0c92db79cc8a2491e9a9d9b762646f859d6b6f
bili22 <bili@bili22.me>
2023-12-25 21:45:54 +0900

    update version code

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,51 @@
-import { Context, Schema } from 'koishi'
+import { readFile } from 'fs/promises';
+import internal from 'stream';
+import { fileURLToPath } from 'url';
+import FormData from 'form-data';
+import { Context, Schema, Service } from 'koishi';
 
-export const name = 'server-temp-kook'
+declare module 'koishi' {
+    interface Context {
+        'server.temp': TempService;
+    }
+}
 
-export interface Config {}
+export const name = 'server-temp-kook';
 
-export const Config: Schema<Config> = Schema.object({})
+export interface Config {
+    token: string;
+}
 
-export function apply(ctx: Context) {
-  // write your plugin here
+export const Config: Schema<Config> = Schema.object({
+    token: Schema.string().required().role('secret'),
+});
+
+export class TempService extends Service {
+    protected token: string;
+
+    constructor(ctx: Context, config: Config) {
+        super(ctx, 'server.temp', true);
+        this.token = config.token;
+    }
+
+    async create(data: Buffer | string | internal.Readable): Promise<{ url: string }> {
+        if (typeof data === 'string') {
+            if (new URL(data).protocol === 'file:') {
+                data = await readFile(fileURLToPath(data));
+            } else {
+                data = await this.ctx.http.get(data, { responseType: 'stream' });
+            }
+        }
+        const payload = new FormData();
+        payload.append('file', data, { filename: 'image.jpg' });
+        const result = await this.ctx.http.post('https://www.kookapp.cn/api/v3/asset/create', payload, { headers: { Authorization: `Bot ${this.token}` } });
+        if (result.code === 0) {
+            return { url: result.data.url };
+        }
+        throw new Error(result.message);
+    }
+}
+
+export function apply(ctx: Context, config: Config) {
+    ctx.plugin(TempService, config);
 }
